fed8e07d653e9f22e75b8293ec8a3eaf83aaad90
Dima <dmitrijs.visnakovs@inbox.lv>
2023-10-15 14:50:35 +0300

    product list

--- src/features/store.js
+++ src/features/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import categoriesSlice from './categories/categoriesSlice';
+import ProductSlice from './products/ProductSlice';
 
 export const store = configureStore({
     reducer: {
         categories: categoriesSlice,
+        products: ProductSlice,
     },
     devTools: true,
 
